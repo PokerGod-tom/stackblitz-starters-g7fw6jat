2d01951e545f91de640f270d84ff2b37c8f80f60
PokerGod-tom <zihewang56@gmail.com>
2025-04-13 01:35:47 +1000

    .

---
+++ app/committee/page.tsx
@@ -0,0 +1,9 @@
+export default function CommitteePage() {
+    return (
+      <main className="p-6">
+        <h1 className="text-2xl font-bold">Committee Members</h1>
+        <p>View Treasurer, Secretary, Chairperson and elected committee members here.</p>
+      </main>
+    );
+  }
+  
